39fb169ecf5f37d9f2101722135820f991110c0d
lirco <lirco77@gmail.com>
2015-06-21 23:51:51 +0300

    added byTagsFilter

--- public/modules/core/controllers/home.client.controller.js
+++ public/modules/core/controllers/home.client.controller.js
@@ -12,6 +12,17 @@
 		self.notes = Notes.query();
 
 		self.tags = self.authentication.user.tags;
+		self.sortOrder = ['-modified'];
+
+		//helper function for finding objects in array
+		var ObjIndexOf = function(arr, prop, val) {
+			for(var i = 0 ; i < arr.length ; i++) {
+				if (arr[i][prop] === val) {
+					return 1
+				}
+			}
+			return -1;
+		};
 
 		self.click = function(tag) {
 			var index = -1;
@@ -22,14 +33,16 @@
 				}
 			}
 			if (index == -1) {
-				selectedTags.push(tag)
+				selectedTags.push(tag);
+				// filter the notes on view by tag selected
+				//TODO: option to show tags of multiple tags selected
+				self.byTagsFilter = function(note) {
+					return ObjIndexOf(note.tags, "text", tag.text) !==-1
+				}
 			} else {
 				selectedTags.splice(index,1);
 			}
 		};
-
-		self.sortOrder = ['-modified'];
-		//self.byTagsFilter =
 
 	}
 
