c753644b56ac0a9a21014c092213634870ee7b6f
lirco <lirco77@gmail.com>
2016-03-01 19:45:48 +0200

    cliptoBox in progress

--- chrome_extension/contentScript.js
+++ chrome_extension/contentScript.js
@@ -1,3 +1,4 @@
+
 chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {
   if (request.method == "getSelection")
     sendResponse({data: window.getSelection().toString()});
@@ -5,36 +6,85 @@ chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {
     sendResponse({}); // snub them.
 });
 
+///**
+// * Get the selected text and wrap it with span
+// * @param document
+// */
+//function getSelectionText() {
+//  var selection,
+//    selectedText,
+//    span;
+//  if (window.getSelection  && window.getSelection().toString() !== '') {
+//    selection = window.getSelection().getRangeAt(0);
+//    selectedText = selection.extractContents();
+//    span = document.createElement("span");
+//    span.setAttribute('class', 'cliptoText');
+//    span.style.backgroundColor = "yellow";
+//    span.appendChild(selectedText);
+//    selection.insertNode(span);
+//    $("<span class='cliptoBox'></span>").insertBefore(".cliptoText");
+//    $(".cliptoBox").animate({top:e.pageX, left:e.pageY})
+//  }
+//  //else if (document.getSelection) {
+//  //  selection = document.getSelection();
+//  //}
+//  // selection.toString() !== '' && alert('"' + selection.toString() + '" was selected at ' + e.pageX + '/' + e.pageY);
+//  //selection.wrap('<span class = "cliptotest" ></span>')
+//}
 
-$(document.body).bind('mouseup', function(e){
+
+function getSelectionInsertSpan() {
   var selection,
-      selectedText,
       span;
+
+  //move this out to global variable
+  var cliptoBoxHtml =
+    '<div class="cliptoBox">' +
+      '<div class="cliptoIconWrapper">' +
+        '<div class="clipIcon">' +
+        '</div>' +
+      '</div>' +
+      '<div class="cliptoIconWrapper">' +
+        '<div class="noteIcon">' +
+        '</div>' +
+      '</div>' +
+      '<div class="cliptoIconWrapper">' +
+        '<div class="facebookIcon">' +
+        '</div>' +
+      '</div>' +
+      '<div class="cliptoIconWrapper">' +
+        '<div class="twitterIcon">' +
+        '</div>' +
+      '</div>' +
+    '</div>';
 
   if (window.getSelection && window.getSelection().toString() !== '') {
     selection = window.getSelection().getRangeAt(0);
-    selectedText = selection.extractContents();
     span = document.createElement("span");
-    span.setAttribute('class', 'cliptoText');
-    span.style.backgroundColor = "yellow";
-    span.appendChild(selectedText);
+    span.setAttribute('id', 'cliptoHelperSpan');
     selection.insertNode(span);
-    $("<span class='cliptoBox'></span>").insertBefore(".cliptoText");
-    $(".cliptoBox").animate({top:e.pageX, left:e.pageY})
+    $("#cliptoHelperSpan").append($(cliptoBoxHtml));
   }
-  //else if (document.getSelection) {
-  //  selection = document.getSelection();
-  //}
+}
 
-  // selection.toString() !== '' && alert('"' + selection.toString() + '" was selected at ' + e.pageX + '/' + e.pageY);
+/**
+ * Insert a span before the selected text
+ * this is done for positioning it relative
+ */
+function insertCliptoBox() {
+  if ($('#cliptoHelperSpan').length) {
+    $('#cliptoHelperSpan').remove();
+    getSelectionInsertSpan();
+  } else {
+    getSelectionInsertSpan();
+  }
+}
 
-  //selection.wrap('<span class = "cliptotest" ></span>')
+$(document.body).bind('mouseup', function(e){
+  //var coords = getSelectionCoords();
+  insertCliptoBox();
+  //$("<span class='cliptoBox'></span>").insertBefore(".cliptoText");
+
+  $(".cliptoBox").animate({top:-45, left:-20});
 
 });
-
-
-
-console.log('***************************************');
-console.log('ha ha ha');
-console.log('***************************************');
-
